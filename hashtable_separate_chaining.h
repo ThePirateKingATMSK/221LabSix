67cd55bed04b462daea1815051dd037b000d25b7
tpk <irondextros@gmail.com>
2022-04-10 15:17:42 -0500

    Wrote some more, need to figure out the indexing of this.

--- hashtable_separate_chaining.h
+++ hashtable_separate_chaining.h
@@ -21,8 +21,18 @@ private:
     // A vector containing the lists
     std::vector<std::list<Key>> *table;
     int currentSize;
-    int buckets;
+    int bucketCount;
     int maxLoad;
+
+    // Helper function to find the bucket index that a certain key should belong to
+    int findSpot(const key_type& key);
+
+    // Helper function to find what the hash of the key should be
+    int hashKey(const key_type& key) {
+        // Simple mod hash
+        //return (key % 10);
+        return 1;
+    };
 
 public:
     HashTable();
@@ -60,10 +70,10 @@ public:
 template<class Key, class Hash>
 HashTable<Key, Hash>::HashTable() {
     // Initialize our default values and create the hash table
-    buckets = 11;
+    bucketCount = 11;
     currentSize = 0;
     maxLoad = 1;
-    table = new std::vector<std::list<Key>>[buckets];
+    table = new std::vector<std::list<Key>>[bucketCount];
 }
 
 // Copy constructor, makes one has table identical to the other
@@ -71,9 +81,11 @@ template<class Key, class Hash>
 HashTable<Key, Hash>::HashTable(const HashTable &other) {
 
     // Copy the variables over
-    buckets = other.buckets;
+    bucketCount = other.buckets;
     maxLoad = other.maxLoad;
     currentSize = other.currentSize;
+    table = new std::vector<std::list<Key>>[bucketCount];
+
 
     for (int i = 0; i < other.buckets; i++) {
         // Use the list's copy assignment to copy the whole list
@@ -97,7 +109,7 @@ HashTable<Key, Hash> &HashTable<Key, Hash>::operator=(const HashTable &other) {
     }
 
     // Copy the variables and all of the lists
-    buckets = other.buckets;
+    bucketCount = other.bucketCount;
     maxLoad = other.maxLoad;
     currentSize = other.currentSize;
 
@@ -105,16 +117,17 @@ HashTable<Key, Hash> &HashTable<Key, Hash>::operator=(const HashTable &other) {
         // Use the list's copy assignment to copy the whole list
         table[i].operator=(other.table[i]);
     }
+    return *this;
 }
 
 // Paramaterized constructor that will allow the user to set the amount of buckets
 template<class Key, class Hash>
 HashTable<Key, Hash>::HashTable(HashTable::size_type buckets) {
     // Set our bucket size and initialize the other variables
-    buckets = buckets;
+    bucketCount = buckets;
     currentSize = 0;
     maxLoad = 1;
-    table = new std::vector<std::list<Key>>[buckets];
+    table = new std::vector<std::list<Key>>[bucketCount];
 
 }
 
@@ -123,8 +136,8 @@ template<class Key, class Hash>
 bool HashTable<Key, Hash>::is_empty() const {
 
     // Iterate through each bucket and see if it contains anything, if it does, return false
-    for (int i = 0; i < buckets; i++){
-        if (table[i] == NULL) {
+    for (int i = 0; i < bucketCount; i++){
+        if (table[i].empty()) {
             return false;
         }
     }
@@ -141,31 +154,135 @@ size_t HashTable<Key, Hash>::size() const {
 // Function to completely empty out the hash table
 template<class Key, class Hash>
 void HashTable<Key, Hash>::make_empty() {
-    for (int i = 0; i < buckets; i++) {
-        // Destroy the list
+    for (int i = 0; i < bucketCount; i++) {
+        // Destroy the list in each bucket
         std::destroy(table[i].begin(), table[i].end());
-        // Set each linked list equal to null as we go
-        table[i] = NULL;
     }
-
 }
 
 // Inserts the given value into the hash table, and rehashes if the maximum load factor is exceeded
 template<class Key, class Hash>
 bool HashTable<Key, Hash>::insert(const value_type &value) {
-    return false;
+
+
+    int index = hashKey(value);
+
+    std::list<Key> bucketList;
+    // Check if the value already exists
+    for (int i = 0; i < bucketCount; i++) {
+        std::cout << table[i].size() << std::endl;
+    }
+
+
+    // It's already a vector of lists, so we can just push the value back once we've found the index.
+    std::cout << table[index].size() << std::endl;
+    currentSize += 1;
+
+    // Check if we have to rehash
+    if (load_factor() > maxLoad) {
+        rehash(currentSize);
+    }
+
+    return true;
 }
 
-// Removes the given value from the hash table
+// Checks if an element exists in a hash table and removes it if it does, or does nothing if it's not present
 template<class Key, class Hash>
 size_t HashTable<Key, Hash>::remove(const key_type &key) {
+    for (int i = 0; i < bucketCount; i++) {
+        // Check to see if we can find the element
+        if (i == 2) {
+            // Use the same function to find the element again and remove it from the list
+            //table[i].remove(table[i].find(table[i].begin(), table[i].end(), key));
+            currentSize -= 1;
+            return 1;
+        }
+    }
+
+    // If we exit the loop without having returned, then the item didn't exist, so return 0
     return 0;
 }
 
 // Returns true or false depending on whether the hashtable contains the given value or not
 template<class Key, class Hash>
 bool HashTable<Key, Hash>::contains(const key_type &key) {
+
+    // Check the list in each bucket for the given key
+    for (int i = 0; i < bucketCount; i++) {
+        if (table[i].find(table[i].begin(), table[i].end(), key)) {
+            return true;
+        }
+    }
+
+    // If we make it out of that loop, we didn't find the element so return false
     return false;
 }
+
+// Function to return the number of buckets in a table
+// WORKING
+template<class Key, class Hash>
+size_t HashTable<Key, Hash>::bucket_count() const {
+    return bucketCount;
+}
+
+
+// Function to return the number of items in a given bucket
+template<class Key, class Hash>
+size_t HashTable<Key, Hash>::bucket_size(size_t n) const {
+    // Check if the index is within bounds
+    if (n >= bucketCount) {
+        throw std::out_of_range("Bucket index is out of range!");
+    }
+
+    // Return the size (amount of items) in that bucket
+    return table[n].size();
+}
+
+// Function that returns the index of the bucket containing the key, or the bucket that would contain it if it existed.
+template<class Key, class Hash>
+size_t HashTable<Key, Hash>::bucket(const key_type &key) const {
+    return 0;
+}
+
+// Function to calculate and return the current load factor
+template<class Key, class Hash>
+float HashTable<Key, Hash>::load_factor() const {
+    // bucketCount / currentSize to calculate the current load factor
+    if (currentSize == 0) {
+        return 0;
+    }
+
+    // Return the calculation if we're not dividing by zero.
+    return (float(bucketCount) / currentSize);
+}
+
+// Function to return the maximum load for that hashtable
+template<class Key, class Hash>
+float HashTable<Key, Hash>::max_load_factor() const {
+    return maxLoad;
+}
+
+// Function to set a new maximum load factor, and rehash if necessary
+template<class Key, class Hash>
+void HashTable<Key, Hash>::max_load_factor(float mlf) {
+
+    maxLoad = mlf;
+
+    // Check if we've exceeded our new load factor
+    if (((float(bucketCount)) / currentSize) > maxLoad) {
+        rehash(currentSize);
+    }
+}
+
+// Function to rehash the table when necessary
+template<class Key, class Hash>
+void HashTable<Key, Hash>::rehash(HashTable::size_type count) {
+    }
+
+template<class Key, class Hash>
+void HashTable<Key, Hash>::print_table(std::ostream &os) const {
+
+}
+
 
 #endif  // HASHTABLE_SEPARATE_CHAINING_H
