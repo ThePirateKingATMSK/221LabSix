88baf73d146557d3d85e3bbba73e32e6207b4eb3
tpk <irondextros@gmail.com>
2022-04-10 12:01:55 -0500

    Completed the constructors and destructor

--- hashtable_separate_chaining.h
+++ hashtable_separate_chaining.h
@@ -21,6 +21,8 @@ private:
     // A vector containing the lists
     std::vector<std::list<Key>> *table;
     int currentSize;
+    int buckets;
+    int maxLoad;
 
 public:
     HashTable();
@@ -54,19 +56,55 @@ public:
     // bool insert(value_type&& value);
 };
 
+// Default constructor, initializes a hash table with 11 buckets
 template<class Key, class Hash>
 HashTable<Key, Hash>::HashTable() {
-
+    // Initialize our default values and create the hash table
+    buckets = 11;
+    currentSize = 0;
+    maxLoad = 1;
+    table = new std::vector<std::list<Key>>[buckets];
 }
 
+// Copy constructor, makes one has table identical to the other
 template<class Key, class Hash>
 HashTable<Key, Hash>::HashTable(const HashTable &other) {
 
+    // Copy the variables over
+    buckets = other.buckets;
+    maxLoad = other.maxLoad;
+    currentSize = other.currentSize;
+
+    for (int i = 0; i < other.buckets; i++) {
+        // Use the list's copy assignment to copy the whole list
+        table[i].operator=(other.table[i]);
+        }
 }
 
 template<class Key, class Hash>
 HashTable<Key, Hash>::~HashTable() {
+    // We created a vector of lists, so we need to delete[]
+    delete[] table;
+}
 
+// Copy assignment operator, used to copy hashtables whilst also checking for self assignment
+template<class Key, class Hash>
+HashTable<Key, Hash> &HashTable<Key, Hash>::operator=(const HashTable &other) {
+
+    // Check for self assignment
+    if (this == &other) {
+        return *this;
+    }
+
+    // Copy the variables and all of the lists
+    buckets = other.buckets;
+    maxLoad = other.maxLoad;
+    currentSize = other.currentSize;
+
+    for (int i = 0; i < other.buckets; i++) {
+        // Use the list's copy assignment to copy the whole list
+        table[i].operator=(other.table[i]);
+    }
 }
 
 #endif  // HASHTABLE_SEPARATE_CHAINING_H
